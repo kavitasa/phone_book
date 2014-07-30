ad5c5662d9d10cc11caa49b171bf53aeaee34f90
Coding <kavita@Kavitas-MacBook-Pro.local>
2014-07-30 14:11:07 -0600

    commit final project really

---
+++ test/phone_book_test.rb
@@ -0,0 +1,32 @@
+gem 'minitest', '~> 5.2'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'minitest/mock'
+require_relative '../lib/phone_book'
+
+class PhoneBookTest < Minitest::Test
+  attr_reader :repository
+  def setup
+    @repository = Minitest::Mock.new
+  end
+  def test_lookup_by_last_name
+    phone_book = PhoneBook.new(repository)
+    repository.expect(:find_by_last_name, [], ["Smith"])
+    phone_book.lookup('Smith')
+    repository.verify
+  end
+
+  def test_lookup_by_last_name_first_name
+    phone_book = PhoneBook.new(repository)
+    repository.expect(:find_by_first_and_last_name, [], ["Alice","Smith"])
+    phone_book.lookup('Smith, Alice')
+    repository.verify
+  end
+
+  def test_lookup_by_number
+    phone_book = PhoneBook.new(repository)
+    repository.expect(:find_by_number, [], ["(123) 123-1234"])
+    phone_book.reverse_lookup('(123) 123-1234')
+    repository.verify
+  end
+end
